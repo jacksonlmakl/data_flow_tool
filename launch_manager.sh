ddbddf585e910ad1b779ea81695a9e3c78b0f4d8
Jackson Makl <jackson.makl@dataiku.com>
2024-06-26 22:39:52 -0400

    f

--- launch_manager.sh
+++ launch_manager.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
+sudo apt-get install python3-pip
 sudo apt-get install python3-venv
 
 python3 -m venv ui_venv
 source ui_venv/bin/activate
-pip3 install Flask
+pip3 install flask
 python3 app.py &
